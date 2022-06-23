e289020c2d4b9ef315a27acba8111abb98133500
Ridwan Sulaiman <105246901+surereal4@users.noreply.github.com>
2022-06-23 17:59:33 +0100

    Fisrt commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let num1 = Number(prompt("Enter First number"));
+let num2 = Number(prompt("Enter Second number"));
+
+let operator = prompt("Enter operator (either +, -, * or /)")
+
+if (operator === '+') {
+    alert("Addition =" + (num1 + num2))
+}
+
+if (operator === '-') {
+    alert("Substraction =" + (num1 - num2))
+}
+
+if (operator === '*') {
+    alert("Multiplication =" + (num1 * num2))
+}
+
+if (operator === '/') {
+    alert("Division =" + (num1 / num2))
+} else{
+    alert("Invalid operator")
+}
