e332eaa745e2f9848628e1ffae7ca7ae75445bdc
Ridwan Sulaiman <105246901+surereal4@users.noreply.github.com>
2022-07-03 20:45:08 +0100

    Edit

--- calculator.js
+++ calculator.js
@@ -1,22 +1,22 @@
 let num1 = Number(prompt("Enter First number"));
 let num2 = Number(prompt("Enter Second number"));
 
-let operator = prompt("Enter operator (either +, -, * or /)")
+let operator = prompt("Enter operator (either +, -, * or /)");
 
 if (operator === '+') {
-    alert("Addition =" + (num1 + num2))
+    alert("Addition =" + (num1 + num2));
 }
 
 if (operator === '-') {
-    alert("Substraction =" + (num1 - num2))
+    alert("Substraction =" + (num1 - num2));
 }
 
 if (operator === '*') {
-    alert("Multiplication =" + (num1 * num2))
+    alert("Multiplication =" + (num1 * num2));
 }
 
 if (operator === '/') {
-    alert("Division =" + (num1 / num2))
+    alert("Division =" + (num1 / num2));
 } else{
-    alert("Invalid operator")
+    alert("Invalid operator");
 }
